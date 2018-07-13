cd8f60ce042c10fc45d5cf213b05c843a7a39695
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-07-13 10:50:50 -0700

    Added collapsible menu to sidebar

--- scripts/layout.js
+++ scripts/layout.js
@@ -23,3 +23,30 @@ class api {
 }
 const recursiveApi = new api();
 recursiveApi.init();
+
+function showStart(){
+    // Can't select all by class?
+    // var iconList = document.getElementsByClassName("start-hide");
+
+    var item1 = document.getElementById('btnSidebarGettingStartedGetConnected');
+    item1.classList.toggle("start-hide");
+    var item2 = document.getElementById('btnSidebarGettingStartedGetPreReqs');
+    item2.classList.toggle("start-hide");
+    var item3 = document.getElementById('btnSidebarGettingStartedGetTheProject');
+    item3.classList.toggle("start-hide");
+    var item4 = document.getElementById('btnSidebarGettingStartedGitItRight');
+    item4.classList.toggle("start-hide");
+}
+
+function showFront(){
+    var item1 = document.getElementById('btnSidebarFrontEndTemplater');
+    item1.classList.toggle("start-hide");
+}
+
+function showBack(){
+    var item1 = document.getElementById('btnSidebarBackEndPreRequisites');
+    item1.classList.toggle("start-hide");
+    var item2 = document.getElementById('btnSidebarBackEndYAMLConfiguration');
+    item2.classList.toggle("start-hide");
+}
+
