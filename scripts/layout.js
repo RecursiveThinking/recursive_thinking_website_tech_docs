e1340fca67c165aad98511176376c90d76295196
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-07-23 12:51:41 -0700

    Added highlighted sidebar links

--- scripts/layout.js
+++ scripts/layout.js
@@ -7,10 +7,10 @@ class api {
     init() {
         // const sidebarItems = document.getElementsByClassName('sidebar-category');
         const sidebarItems = document.querySelectorAll('.sidebar-category');
-        console.log(sidebarItems);
+        // console.log(sidebarItems);
         const sideBarItemsArray = Array.from(sidebarItems);
-        console.log('Array');
-        console.log(sidebarItems);
+        // console.log('Array');
+        // console.log(sidebarItems);
         sideBarItemsArray.forEach((item) => {
             item.addEventListener('click', () => {
                 const pageId = item.getAttribute('data-pageId');
@@ -24,11 +24,45 @@ class api {
 const recursiveApi = new api();
 recursiveApi.init();
 
-function showStart(){
+// Should refactor so one function handles all cases. I'm not sure how to refer to the clicked on element.
+function topFocus1(){
+    Array.from(document.getElementsByClassName("top-category")).forEach(function(item){
+        item.classList.remove("top-focus");
+        var item1 = document.getElementById("about-focus");
+        item1.classList.add("top-focus");
+    });
+}
 
+function topFocus2(){
+    Array.from(document.getElementsByClassName("top-category")).forEach(function(item){
+        item.classList.remove("top-focus");
+        var item1 = document.getElementById("start-focus");
+        item1.classList.add("top-focus");
+    });
+}
+
+function topFocus3(){
+    Array.from(document.getElementsByClassName("top-category")).forEach(function(item){
+        item.classList.remove("top-focus");
+        var item1 = document.getElementById("front-focus");
+        item1.classList.add("top-focus");
+    });
+}
+
+function topFocus4(){
+    Array.from(document.getElementsByClassName("top-category")).forEach(function(item){
+        item.classList.remove("top-focus");
+        var item1 = document.getElementById("back-focus");
+        item1.classList.add("top-focus");
+    });
+}
+
+function showStart(){
+    // toggles display property for sub headings on nav bar
     Array.from(document.getElementsByClassName("start-child")).forEach(function(item){
         item.classList.toggle("start-hide");
     });
+    // toggles icon
     var startPlus = document.getElementById('start');
     startPlus.classList.toggle("fa-minus-square");
 }
