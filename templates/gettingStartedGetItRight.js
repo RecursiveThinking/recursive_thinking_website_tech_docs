1ef6cd0cb6d5c834bfa18ed10c6a17e71519b0a8
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-08-01 11:03:36 -0700

    Added modal popout to all images

--- templates/gettingStartedGetItRight.js
+++ templates/gettingStartedGetItRight.js
@@ -1,27 +1,30 @@
-// function setUpGettingStartedGetItRight()
-// {
+function setUpGettingStartedGetItRight(){
+    // Get the modal
+    var modal = document.getElementById('myModal');
 
-// }
-// var slideIndex = 1;
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var imgs = document.querySelectorAll("img");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    
+    imgs.forEach(function(img){
+        img.addEventListener('click', function(){
+            console.log('clicked');
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        });
+    });
 
-// function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     if (n > slides.length) {slideIndex = 1} 
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none"; 
-//     }
-//     console.log(slides);
-//     slides[slideIndex-1].style.display = "block"; 
-// }
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// showSlides(slideIndex);
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }  
 
-// // Next/previous controls
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
+}
 
 
 
