6b942d0677e694b6b743055d5f5bba0bbfe710bf
Seth Borne <sethborne@gmail.com>
2018-08-09 13:50:29 -0700

    updates/changes that failed to fix github response max-age issue

--- templates/whoWeAre.js
+++ templates/whoWeAre.js
@@ -1,6 +1,16 @@
 function setUpWhoWeAre(){
   var card = document.getElementById("whoWeAreCard")
-	file = fetch('https://raw.githubusercontent.com/RecursiveThinking/recursive_thinking_website_tech_docs/master/markdown/whoWeAre.md')
+  let configHeaders = new Headers();
+  configHeaders.append('Cache-Control', 'max-age=20')
+  // console.log(configHeaders);
+  console.log(configHeaders.get('Cache-Control'));
+  let config = {
+    method: 'GET',
+    // headers: configHeaders,
+    cache: "no-cache"
+  }
+	file = fetch('https://raw.githubusercontent.com/RecursiveThinking/recursive_thinking_website_tech_docs/master/markdown/whoWeAre.md', config)
+	// file = fetch('https://raw.githubusercontent.com/RecursiveThinking/recursive_thinking_website_tech_docs/master/markdown/whoWeAre.md')
 		.then(response => response.text())
 		.then(text => md.render(text))
 		.then(renderedHTML => card.innerHTML = renderedHTML);
