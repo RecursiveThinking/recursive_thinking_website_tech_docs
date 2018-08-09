c50c9f00e3286d06f0be4c1966849d1db190eb9b
Seth Borne <sethborne@gmail.com>
2018-08-09 10:39:22 -0700

    update whoWeAre setup to include fetch of md file

--- templates/whoWeAre.js
+++ templates/whoWeAre.js
@@ -1,3 +1,7 @@
 function setUpWhoWeAre(){
-
+  var card = document.getElementById("whoWeAreCard")
+	file = fetch('https://raw.githubusercontent.com/RecursiveThinking/recursive_thinking_website_tech_docs/master/markdown/whoWeAre.md')
+		.then(response => response.text())
+		.then(text => md.render(text))
+		.then(renderedHTML => card.innerHTML = renderedHTML);
 }
