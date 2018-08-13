2531e9ecea0d7a08df67cf4bbf902822d2d60d44
Warren <w.e.hellman@gmail.com>
2018-08-13 14:37:13 -0700

    Added second slideshow and deploy backend JS

--- templates/gettingStartedBackEndDeployStack.js
+++ templates/gettingStartedBackEndDeployStack.js
@@ -1,5 +1,6 @@
 function setUpGettingStartedBackEndDeployStack(){
     showSlides(slideIndex);
+    showSlides1(slideIndex1);
   // Get the modal
   var modal = document.getElementById('myModal');
 
@@ -25,6 +26,9 @@ function setUpGettingStartedBackEndDeployStack(){
   } 
 }
 
+
+// Refactor
+// First Slideshow
 var slideIndex = 1;
 
 function showSlides(n) {
@@ -37,9 +41,40 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block"; 
 }
-
 // Next/previous controls
 function plusSlides(n) {
     document.getElementById('tutorial1').scrollIntoView();
     showSlides(slideIndex += n);
+}
+
+// Second Slideshow
+var slideIndex1 = 1;
+
+function showSlides1(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides1");
+    if (n > slides.length) {slideIndex1 = 1} 
+    if (n < 1) {slideIndex1 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slides[slideIndex1-1].style.display = "block"; 
+}
+// Next/previous controls
+function plusSlides1(n) {
+    document.getElementById('tutorial2').scrollIntoView();
+    showSlides1(slideIndex1 += n);
+}
+
+function expandImg(){
+    let button = document.getElementById('scriptImg');
+    button.classList.toggle('hideImg')
+    let buttonText = document.querySelector('.expand');
+    
+    if (buttonText.innerHTML === 'Show Image'){
+        buttonText.innerHTML = 'Hide Image';
+    }
+    else if(buttonText.innerHTML === 'Hide Image'){
+        buttonText.innerHTML = 'Show Image';
+    }
 }
